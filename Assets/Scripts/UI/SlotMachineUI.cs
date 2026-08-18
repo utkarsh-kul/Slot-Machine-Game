d4145d21d1e0a197009d4cef78a1d13b7b6247be
Utkarsh Kulshreshtha <utkarshkulshreshtha2@gmail.com>
2026-08-18 20:00:49 +0530

    Added the Credits UI and Spin Button Functionality

---
+++ Assets/Scripts/UI/SlotMachineUI.cs
@@ -0,0 +1,14 @@
+using TMPro;
+using UnityEngine;
+
+public class SlotMachineUI : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private SlotMachineEconomy economy;
+    [SerializeField] private TMP_Text creditsText;
+
+    private void Update()
+    {
+        creditsText.text = "Credits: " + economy.CurrentCredits;
+    }
+}
